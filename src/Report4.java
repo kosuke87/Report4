d6957aed0dfb15f36d0a15fbefbed74d201d84e2
Kosuke Hanashiro <e175757@ie.u-ryukyu.ac.jp>
2017-11-27 20:38:43 +0900

    report4

--- src/Report4.java
+++ src/Report4.java
@@ -1,12 +1,8 @@
 
 public class Report4 {
     public static void main(String[] args){
-        String str=null;
-        try{
-            System.out.println(str.length());
-        }catch(NullPointerException e){
-            System.out.println("nullです。");
-            System.out.println(e.getMessage());
-        }
+        String str="3．14";
+        double value= Double.parseDouble(str);
+
     }
 }
